265599725420b682f15e5422b9432b7bedff46f8
Shawn Koski <shawnnkoski@gmail.com>
2018-04-11 22:46:06 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,11 +2,13 @@ require 'pry'
 def alphabetize(arr)
   esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  sorted = []
   
   
-  
- arr.sort_by
- arr
+ arr.sort_by do |phrase|
+   sorted << phrase
+ end
+ sorted
 end
 
 expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
