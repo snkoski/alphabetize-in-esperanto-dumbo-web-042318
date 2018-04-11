507a51de96f776844a65e9161efa3eaf47588e2d
Shawn Koski <shawnnkoski@gmail.com>
2018-04-11 23:03:39 +0000

    sorted arrays that use esperanto strings

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  sorted = []
+  
+  
+ 
+   
+   sorted = arr.sort_by do |phrase|
+     phrase.tr(esp_alph, ascii)
+   end
+ 
+ sorted
+ 
 end
+
+expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
+puts alphabetize(expressions)
